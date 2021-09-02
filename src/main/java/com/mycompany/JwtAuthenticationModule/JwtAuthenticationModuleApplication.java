57ea15277826bebd50f98581606efc6c0ad164ca
Karan Rawlley <karan8798rawlley@gmail.com>
2021-09-02 11:25:23 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/JwtAuthenticationModule/JwtAuthenticationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.JwtAuthenticationModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/JwtAuthenticationModule/JwtAuthenticationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.JwtAuthenticationModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
